b95b9edf8f6b2a2973a23f34a2b8ff0fd918c8c1
Kato <jyobi0302communication@gmail.com>
2017-04-12 12:17:45 +0900

    add new file

---
+++ thabto2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<head>
+
+<title>Thabto</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Thabto</h1>
+<!-- ナビゲーションバー -->
+<div id="menubar">
+ <ul>
+ <li><a href="ｈttp" title="ホーム" >ホーム</a></li>
+ <li><a href="http" title="サイト概要">サイト概要</a></li>
+ <li><a href="http" title="検索">検索</a></li>
+ <li><a href="http" title="ログイン">ログイン</a></li>
+ <li><a href="http" title="登録">登録</a></li>
+ </ul>
+</div>
+
+		
+
+</body>
+</html>
